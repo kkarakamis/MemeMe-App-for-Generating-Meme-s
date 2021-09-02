671f9733a8c491b7896ad637dba500708387e564
kkarakamis <kutaykarakamis@hotmail.com>
2021-09-02 15:48:16 +0300

    view and buttons placed

---
+++ MemeMe/MemeImageController.swift
@@ -0,0 +1,24 @@
+//
+//  ImageController.swift
+//  MemeMe
+//
+//  Created by Kutay Karakamış on 2.09.2021.
+//
+//
+import Foundation
+import UIKit
+
+class MemeImageControl: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+
+    var image: UIImage!
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.allowsEditing = true
+        if let img = info[.originalImage] {
+            self.image = (img as! UIImage)
+        }
+        //self.imageView.contentMode = .scaleAspectFit
+        picker.dismiss(animated: true, completion: nil)
+    }
+
+}
--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -7,13 +7,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    
+    var imageDelegator: MemeImageControl = MemeImageControl()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+
     }
+    
+//    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+//        picker.allowsEditing = true
+//        if let img = info[.originalImage] {
+//            self.imageView.image = (img as! UIImage)
+//        }
+//        self.imageView.contentMode = .scaleAspectFit
+//        picker.dismiss(animated: true, completion: nil)
+//    }
+    
+    @IBAction func selectAnImage(_ sender: Any) {
+        let selectImageController = UIImagePickerController()
+        selectImageController.delegate = self.imageDelegator
+        selectImageController.sourceType = .photoLibrary
+        present(selectImageController, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func selectAnImageFromAlbum(_ sender: Any) {
+        let selectImageController = UIImagePickerController()
+        selectImageController.delegate = self.imageDelegator
+        selectImageController.sourceType = .photoLibrary
+        present(selectImageController, animated: true, completion: nil)
+    }
+    
 
-
+    @IBAction func selectAnImageFromCamera(_ sender: Any) {
+        let selectImageController = UIImagePickerController()
+        selectImageController.delegate = self.imageDelegator
+        selectImageController.sourceType = .camera
+        present(selectImageController, animated: true, completion: nil)
+    }
+    
 }
 
